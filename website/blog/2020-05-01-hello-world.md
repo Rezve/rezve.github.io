b7ef5e449b0fc56f3b231860aeac12e6d9137202
Md. Rezve Hasan <7360054+Rezve@users.noreply.github.com>
2020-05-03 01:40:17 +0600

    updated about section

--- website/blog/2020-05-01-hello-world.md
+++ website/blog/2020-05-01-hello-world.md
@@ -3,6 +3,6 @@ title: Hello World
 author: Md. Rezve Hasan
 ---
 
-Setting up a static web site using [docusaurus](https://docusaurus.io/en/). This is really a great tool to set up simple static website. It uses markdown and react which makes this very extendable.
+Just completed setting up a static web site using [docusaurus](https://docusaurus.io/en/). This is really a great tool to set up simple static website. It uses markdown and react which makes this very extendable.
 
 <!--truncate-->
--- website/pages/en/about.js
+++ website/pages/en/about.js
@@ -36,6 +36,18 @@ function About(props) {
     },
   ];
 
+  const imageStyle = {
+    float: 'left',
+    paddingRight: '20px',
+    width: '220px'
+  };
+
+  const skillStyle = {
+    listStyleType: 'circle', 
+    float: 'left',
+    paddingLeft: '20px'
+  }
+
   return (
     <div className="docMainWrapper wrapper">
       <Container className="mainContainer documentContainer postContainer">
@@ -43,10 +55,23 @@ function About(props) {
           <header className="postHeader">
             <h1>About Me</h1>
           </header>
-          <p>
+          <p style={{fontSize: '18px'}}>
+            <img src={`${baseUrl}img/rezve_hasan.jpg`} style={imageStyle} alt="Md. Rezve Hasan" />
             Hi, I am Md. Rezve Hasan. Enthusiastic software engineer with 2+ years of experience participating<br/>
             in the complete product development lifecycle of successfully launched applications. <br/>
             My current stack is Node.js, TypeScript, MongoDB, Angular etc. </p>
+
+            <h6>TECHNICAL SKILLS</h6>
+            
+            <ul style={skillStyle}>
+              <li>Node.js – Express.js, Mongoose, Mocha, Chai, Istanbul, Webpack, Babel </li>
+              <li>PHP – Wordpress, CodeIgniter ( Helper, Hook, HMVC, MVC)</li>
+              <li>Java – Android, Socket Programming (TCP, UDP), Servlet, JSP</li>
+              <li>MySQL (Stored Procedure, Indexing, Trigger, CLI, MySQL Workbench)</li>
+              <li>NoSQL (MongoDB, CLI), SQLite</li>
+              <li>Angular (Module, Service, Routing, DI)</li>
+              <li>Git</li>
+            </ul>
           {/* <GridBlock contents={supportLinks} layout="threeColumn" /> */}
         </div>
       </Container>
